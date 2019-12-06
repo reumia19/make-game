4e84c73e622284c8c5341ac9ee30f1b8c7d1050e
reumia19 <56555888+reumia19@users.noreply.github.com>
2019-12-06 15:35:58 +0900

    fix : window size,rect size

--- sketch.java
+++ sketch.java
@@ -3,13 +3,11 @@ import processing.serial.*;
 Serial myPort;
 
 int myString = 0;
-int xSize = 500;
-int ySize = 300;
 int mySize = 50;
 
 void setup(){
   myPort = new Serial(this,Serial.list()[1],9600); 
-  size(xSize,ySize);
+  size(500,300);
 
 }
 
@@ -24,8 +22,9 @@ void readArduino(){
     myString = myPort.read();
     println(myString);
 }
+}
 
 void drawPlayer(){
-rect(xSize/2,ySize -100,mySize,mySize);
+rect(width,height-100,mySize,mySize);
 
 }
